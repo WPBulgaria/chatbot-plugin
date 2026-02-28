545c6d831c504f78084da49e6c259f4ee708bd12
Sashe Vuchkov <sashevuchkov@gmail.com>
2026-03-01 00:03:50 +0200

    assigning chatbot plans bug

--- wpb-chatbot.php
+++ wpb-chatbot.php
@@ -5,7 +5,7 @@ Plugin Name: WPBulgaria Chatbot
 Plugin URI: https://wpbulgaria.com
 Description: A chatbot for WPBulgaria.
 Author: Sashe Vuchkov
-Version: 0.0.25
+Version: 0.0.26
 Author URI: https://wpbulgaria.com
 Text Domain: wpbulgaria-chatbot
 
